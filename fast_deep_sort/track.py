32e9876ea9c9f14c7421e1d26946bb266a59bb2c
Aaron <aaronrmm@gmail.com>
2020-02-10 15:33:15 -0500

    added feature extraction to track assignment

--- fast_deep_sort/track.py
+++ fast_deep_sort/track.py
@@ -1,3 +1,7 @@
+import numpy as np
+import tensorflow as tf
+
+
 class Detection:
     def __init__(self, frame_idx, detection_box, features=None):
         self.frame_idx = frame_idx
@@ -25,58 +29,108 @@ class Detection:
         mask = np.zeros((height, width, 3))
         mask[bottom(height):top(height),left(width):right(width)]=1
         return mask
-
-
+        
+        
 class Track:
-    def __init__(self, first_detection):
-        self.detections = {}
-        self.add_detection(first_detection)
-        self.last_detection = first_detection
-        
-    def add_detection(self, detection):
-        self.detections[detection.frame_idx] = detection
-        self.last_detection = detection
-        
-    def is_expired(self, current_frame, expiration_threshold):
-        return (current_frame - self.get_last_detection().frame_idx) > expiration_threshold
-        
-    def get_last_detection(self):
-        return self.last_detection
-        
-        
-class TrackManager:
-        
-    def __init__(self, expiration_threshold=2):
-        self.expiration_threshold = expiration_threshold
-        self.active_tracks = []
-        self.all_tracks = []
-        
-    def new_track(self, detection):
-        track = Track(detection)
-        self.all_tracks.append(track)
-        return track
-        
-    def update_tracks(self,frame_idx, detections, matchmaker):
-        found_tracks = []
-        unassigned_tracks = self.active_tracks
-        occluded_tracks = []
-        detections = [Detection(frame_idx, detection_box, None) for detection_box in detections]
-        matches: dict = matchmaker.match(detections, self.all_tracks)
-        for detection in detections:
-            track:Track = None
-                
-            # if no match found, make new Track
-            if not detection in matches.keys():
-                track = self.new_track(detection)
-            else:
-                track = matches[detection]
-                
-            found_tracks.append(track)
-            
-        # only keep unassigned tracks that haven't expired in active_tracks
-        for track in unassigned_tracks:
-            if not track.is_expired(frame_idx, self.expiration_threshold):
-                occluded_tracks.append(track)
-        self.active_tracks = occluded_tracks + found_tracks
+    def __init__(self, track_id, class_id, buffer_idx, frame_idx, initial_bbox):
+        self.buffer_idx = buffer_idx # set to -1 when inactive
+        self.track_id = track_id
+        self.last_frame = frame_idx
+        self.detections = {frame_idx:Detection(frame_idx, initial_bbox)}
+        self.class_id = class_id
 
-        return found_tracks
+    def add_detection(self, frame_idx, bbox):
+        self.detections[frame_idx] = Detection(frame_idx, bbox)
+        self.last_frame = frame_idx
+
+    def is_active(self):
+        return self.buffer_index != -1
+    
+    def get_last_detection(self):
+        return self.detections[self.last_frame]
+    
+    
+class TrackManager:
+    def __init__(self, max_tracks=32, feature_dim=128, infinite_distance= 1000):
+        self.next_track_id = 0
+        self.last_frame_idx = 0
+        self.all_tracks = []
+        self.active_tracks = []
+        self.active_track_features = np.full((max_tracks, feature_dim), fill_value =infinite_distance, dtype=np.float32)
+        self.usable_buffer_slots = list(range(max_tracks))
+        
+    def add_track(self, frame_idx, initial_bbox, features):
+        # assign slot in buffer
+        empty_buffer_idx = self.usable_buffer_slots.pop()
+        track = Track(
+            track_id = self.next_track_id,
+            class_id = 0, # TODO support multiple classes
+            buffer_idx = empty_buffer_idx,
+            frame_idx = frame_idx,
+            initial_bbox = initial_bbox)
+        self.next_track_id+=1
+        self.all_tracks.append(track)
+        self.active_tracks.append(track)
+        self.active_track_features[empty_buffer_idx] = features
+    
+    def update_tracks(self, sess, frame_batch, bbox_batch, class_batch, distance_metric, in_img, emb, infinite_distance= 1000):
+        INPUT_HEIGHT = 256
+        INPUT_WIDTH = 128
+        in_img = sess.graph.get_tensor_by_name('input:0')
+        print('frame batch',frame_batch.shape)
+        emb = sess.graph.get_tensor_by_name('head/out_emb:0')
+        for row_idx, frame_idx in enumerate(range(self.last_frame_idx, self.last_frame_idx+frame_batch.shape[0])):
+            print('frame index', frame_idx)
+            # resize detection images
+            detection_images = resize_images(bbox_batch[row_idx], frame_batch[row_idx])
+            detection_images = tf.image.resize_images(detection_images, [INPUT_HEIGHT, INPUT_WIDTH])
+            # extract features -> np.array(num_detections, embedding_dim)
+            new_detection_features = sess.run(emb, feed_dict={in_img: detection_images.eval()})
+            distance = distance_metric(  # num_detections x num_tracks
+                new_detection_features, # num_detections x embedding size
+                self.active_track_features # num_tracks x embedding size
+            )
+            # find best detection for each active track
+            for active_track in self.active_tracks:
+                best_detection_index = np.argmin(distance[:, active_track.buffer_idx])
+                
+                # add detection to track
+                active_track.add_detection(frame_idx, bbox_batch[row_idx, best_detection_index])
+                active_track.last_frame = frame_idx #TODO this and previous step can be combined in setter
+                
+                # remove detection from distance metric
+                distance[best_detection_index, :] = infinite_distance
+                
+            # create new tracks for unmatched detections
+            for detection_idx, detection in enumerate(distance):
+                if detection[0]!=infinite_distance: # is unmatched detection
+                    self.add_track(
+                        frame_idx = frame_idx,
+                        initial_bbox= bbox_batch[row_idx, detection_idx],
+                        features= new_detection_features[detection_idx])
+        
+        # TODO remove expired tracks from active track list and buffer
+        expired_tracks = []
+        # TODO get expired tracks
+        for track in expired_tracks:
+            self.active_tracks.remove(track)
+            self.usable_buffer_slots.append(track.buffer_idx)
+            self.active_track_features[track.buffer_idx,:] = infinite_distance
+            track.buffer_idx = -1
+        
+        self.last_frame_idx += frame_batch.shape[0]
+        return distance, new_detection_features, self.all_tracks # for debugging
+        
+
+def extract_crops(detection_boxes, image_numpy):
+    image_height = image_numpy.shape[0]
+    image_width = image_numpy.shape[1]
+    image_channels = image_numpy.shape[2]
+    h = np.arange(image_width)#*b.shape[2]).reshape(b.shape[1],b.shape[2])
+    v = np.arange(image_height)
+    mask = (detection_boxes[:,1, None]*image_width <=h) & (detection_boxes[:,3, None]*image_width >= h)
+    mask = np.stack([mask for _ in range(image_height)], axis=1)
+    mask[(detection_boxes[:,0, None]*image_height > v) | (detection_boxes[:,2, None]*image_height < v)]=False
+    mask = np.stack([mask for _ in range(image_channels)], axis=3)
+    return image_numpy*mask
+resize_images = extract_crops
--- main.py
+++ main.py
@@ -6,41 +6,68 @@ from importlib import import_module
 from fast_deep_sort.preprocess import image_dir_batch_iterator
 from fast_deep_sort.detection import load_detector
 from fast_deep_sort.track import TrackManager
+from fast_deep_sort.distance_metrics import pairwise_cosine_distance as distance_metric 
 
 
 FRAMES_DIR = Path('input_frames')
 IMAGE_FORMATS = ['jpg']
 DETECTOR_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
-BATCH_SIZE = 64
+BATCH_SIZE = 12
 DETECTION_THRESHOLD = 0.5
 EXPIRATION_THRESHOLD=10
+EMBEDDING_DIM = 128
+INFINITE_DISTANCE = 1000
+CHECKPOINT_PATH = Path('./triplet_reid/market1501_weights')
+FEATURE_EXTRACTOR_PATH = './triplet_reid/encoder_trinet.pb'
+INPUT_HEIGHT = 256
+INPUT_WIDTH = 128
+SIZE_ACTIVE_TRACK_BUFFER = 32
 
-def generate_tracks(matchmaker):
-    batch_iterator = image_dir_batch_iterator(FRAMES_DIR, IMAGE_FORMATS, BATCH_SIZE)
-    detector = load_detector(DETECTOR_NAME)
-    tracker = TrackManager(expiration_threshold = EXPIRATION_THRESHOLD)
+def generate_tracks():
 
     frames_processed = 0
-    with tf.Session():
+    # check memory usage of model with session config
+    config = tf.ConfigProto(log_device_placement=False)
+    # config.gpu_options.per_process_gpu_memory_fraction = 0.1
+    config.gpu_options.allow_growth = True
+    with tf.Session(graph=tf.Graph(), config=config) as sess:
+    
+        batch_iterator = image_dir_batch_iterator(FRAMES_DIR, IMAGE_FORMATS, BATCH_SIZE)
+        with tf.device('/cpu:0'):
+            detector = load_detector(DETECTOR_NAME)
+        tracker = TrackManager(max_tracks = SIZE_ACTIVE_TRACK_BUFFER, feature_dim = EMBEDDING_DIM, infinite_distance = INFINITE_DISTANCE)
+        
+        #load weights for triplet_reid
+        output_graph_def = tf.GraphDef()
+        with open(FEATURE_EXTRACTOR_PATH, "rb") as f:
+            output_graph_def.ParseFromString(f.read())
+            tf.import_graph_def(output_graph_def, name='')
+        print('{} ops in the frozen graph.'.format(len(output_graph_def.node)))
+        in_img = sess.graph.get_tensor_by_name('input:0')
+        emb = sess.graph.get_tensor_by_name('head/out_emb:0')
+        
+        print("%d frames processed..."%(frames_processed))
         while(True):
             try:
-                next_batch = batch_iterator.get_next().eval()
-                batch_size = next_batch.shape[0]
+                #get batch of frames
+                image = batch_iterator.get_next()
+                frame_batch = image.eval()
+                batch_size = frame_batch.shape[0]
                 
-                detection_dict = detector.__call__(next_batch)
+                #get detections
+                detection_dict = detector.__call__(frame_batch)
                 detection_scores = detection_dict["scores"]
                 num_detections = detection_dict["num_detections"]
-                detection_boxes = detection_dict["boxes"]
-                detection_classes = detection_dict["classes"]
+                bbox_batch = detection_dict["boxes"]
+                class_batch = detection_dict["classes"]
+                
                 # filter detections by confidence
                 mask = detection_scores > DETECTION_THRESHOLD
-                # keep detections separated by frame
-                detections_by_frame = [detection_boxes[i][mask[i]] for i in range(batch_size)]
+                max_detections = np.max(np.sum(mask, axis=1))
+                class_batch = class_batch[:,:max_detections]
+                bbox_batch = bbox_batch[:,:max_detections,:]
 
-                # assign tracks to the detections of each frame
-                for frame_idx, detections in enumerate(detections_by_frame):
-                    frame_idx = frame_idx + frames_processed
-                    tracks = tracker.update_tracks(frame_idx, detections, matchmaker)
+                distance, detection_features, tracks = tracker.update_tracks(sess, frame_batch, bbox_batch, class_batch, distance_metric, in_img, emb)
                 frames_processed+=batch_size
                 print("%d frames processed..."%(frames_processed))
             except tf.errors.OutOfRangeError:
@@ -48,22 +75,6 @@ def generate_tracks(matchmaker):
                 break
     return tracker.all_tracks
     
-class Matcher:
-    
-    def match(self, detections, tracks):
-        matches = {}
-        
-        unassigned_tracks = [track for track in tracks]
-        
-        for detection in detections:
-            # track matching algorithm - currently random #TODO use actual algorithm
-            if len(unassigned_tracks)>0:
-                track = unassigned_tracks.pop()
-                track.add_detection(detection)
-                matches[detection]=track
-                
-        return matches
     
 if __name__ == "__main__":
-    matchmaker = Matcher()
-    print(generate_tracks(matchmaker))
+    print(generate_tracks())
